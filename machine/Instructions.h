f1792a965f357ca2b90780bd68ccb7d70527130e
Christian Wittwer <christianwittwer@Christians-MacBook-Air.local>
2024-04-08 16:03:29 -0600

    Machine basics up to step 6

--- machine/Instructions.h
+++ machine/Instructions.h
@@ -9,10 +9,29 @@ public:
     void Encode(int x);
     void Encode(long long int x);
     void Encode(void * p);
-
+	void PushValue(int value);
+	void PrintIntegerLinux64();
+	void WriteMinusLinux64();
+	void WriteSpaceLinux64();
+	void PopAndWrite();
+	void Call(void * function_address);
+	unsigned char * GetAddress();
 
 private:
-	unsigned char mCode[MAX_INSTRUCTIONS]; 
+	unsigned char mCode[MAX_INSTRUCTIONS];
 	int mCurrent; // where we are in mCode
-	int mStartOfMain;
+	int mPrintInteger; 
+		// Location to store an integer about to be printed.
+	int mTempInteger; 
+		// Location to store one char of integer to be printed.
+
+	int mStartOfPrint;
+		// The value of mCurrent where PrintIntegerLinux 64 starts.
+		// Jump to this offset of mCode to print.
+	int mStartOfMain; 
+		// the value of mCurrent after coding the Print functions.
+		// Jump to this offset of mCode to start program execution.
+	char mMinusString; // Holds '-'
+	char mSpaceString; // Holds ' '
+
 };
--- machine/InstructionsTester.cpp
+++ machine/InstructionsTester.cpp
@@ -2,6 +2,12 @@
 
 int main() {
     InstructionsClass code;
+
+    code.PushValue(-500);
+    code.PopAndWrite();
+    code.PushValue(1000);
+    code.PopAndWrite();
+
     code.Finish();
     code.Execute();
     return 0;
